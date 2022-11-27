296d76d7cf9a9907dcc5b4448c825795edef4167
jalalbello <jalaleddinebellouchi@gmail.com>
2022-11-27 21:29:26 +0100

    init repo

--- Project Library/main.js
+++ Project Library/main.js
@@ -1,7 +1,8 @@
+"use strict";
 let myLibrary = [];
 
-var bookNumber = 4
 
+// Start of Book Constructor
 function book(title, author, pages, read) {
     this.title = title
     this.author = author
@@ -12,44 +13,73 @@ function book(title, author, pages, read) {
 book.prototype.info = function() {
     return (`${this.title} by ${this.author}, ${this.pages}, ${this.read}`)
   }
+// End of Book Constructor
 
-const book1 = new book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
-const book2 = new book("The Hobbit", "J.R.R. Tolkien", 294, "not read yet");
-const book3 = new book("The Hobbit", "J.R.R. Tolkien", 293, "not read yet");
-
-function addBookToLibrary(book){
-    myLibrary.push(book)
+// Apend books to the Dom
+const createObject= () => {
+    const pages = document.getElementsByName("pages")[0].value;
+    const author = document.getElementsByName("author")[0].value;
+    const title = document.getElementsByName("title")[0].value;
+    const readValue = document.getElementsByName("read")[0].checked;
+    let read = "";
+    readValue ? read = 'read' : read = "not read yet"
+    const newBook = new book(pages, author, title, read);
+    myLibrary.push(newBook)
 }
 
-addBookToLibrary(book1)
-addBookToLibrary(book2)
-addBookToLibrary(book3)
 
+const render = ()=>{
+    createObject()
+    const section = document.getElementById("displayBook")
+    const div = document.createElement("div")
+    const removeBtn = document.createElement('button');
 
-for (const item of myLibrary){
-    const body = document.querySelector("body");
-    const p = document.createElement('p');
-    p.innerText = item.info();
-    body.appendChild(p);
+    for (let i=0; i<myLibrary.length; i++){
+        section.appendChild(div);
+        div.appendChild(removeBtn)
+        div.setAttribute("id", i);  
+        removeBtn.classList.add("removeBtn");  
+    }
+    let btns = document.getElementsByClassName("removeBtn");
+
+    for(let i = 0; i < btns.length; i++) {
+        if (typeof(btns[i].onclick) == "undefined") {
+            // event undefined
+            btns[i].addEventListener("click", () => {
+            console.log("Clicked index: " + i);
+            })
+          }
+    }
 }
 
+
+
+// if(myLibrary.length > 0){
+//     for (let btn of btns){
+//     btn.addEventListener('click', () => {
+//         myLibrary.splice(myLibrary.indexOf(item),1);
+//         setData()
+//         render();
+//     });
+// }
+
+
+
+console.log(myLibrary.length);
+// Toggle form visibility
 const toggleFormOpen = () =>{
     const form = document.querySelector("form");
     form.classList.toggle("form-toggle");
     console.log("clicked");
 }
 
-const getNewBook= () => {
-    const pages = document.getElementsByName("pages")[0].value;
-    const author = document.getElementsByName("author")[0].value;
-    const title = document.getElementsByName("title")[0].value;
-    const read = document.getElementsByName("read")[0].checked;
-    const book3 = new book("The Hobbit", "J.R.R. Tolkien", 293, "not read yet");
-}
-
+// Prevent Form Submition
 const element = document.querySelector('form');
 element.addEventListener('submit', event => {
   event.preventDefault();
   // actual logic, e.g. validate the form
   console.log('Form submission cancelled.');
 })
+
+
+
---
+++ Project Tic Tac Toe/main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const Gameboard = () => {
+    let gameBoard = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0],
+    ]
+    console.log(gameBoard[1]);
+    console.log(gameBoard);
+  };
+  Gameboard()
